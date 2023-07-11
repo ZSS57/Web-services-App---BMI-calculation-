ef4687d85641235a719e4a4ed6f37e8b93ac08ba
ZSS57 <101138757+ZSS57@users.noreply.github.com>
2023-07-10 22:38:00 -0700

    Add files via upload

---
+++ RestBMI/IService1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestBMI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+
+        [WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "/myBMI?height={height}&weight={weight}")]
+        double myBMI(int height, int weight);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+        ResponseFormat = WebMessageFormat.Json, UriTemplate = "/myHealthJson?height={height}&weight={weight}")]
+
+        result myHealthJson(int height, int weight);
+
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+       ResponseFormat = WebMessageFormat.Xml, UriTemplate = "/myHealthXml?height={height}&weight={weight}")]
+
+        result myHealthXml(int height, int weight);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+
+    public class result
+    {
+
+
+        [DataMember(Order = 1)]
+        public double bmi { get; set; }
+        [DataMember(Order = 2)]
+        public string risk { get; set; }
+
+        [DataMember(Order = 3)]
+        public string more { get; set; }
+    }
+
+}
---
+++ RestBMI/Service1.svc.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RestBMI
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+
+        public double myBMI(int height, int weight)
+        {
+            double myBMI = 0;
+            myBMI = (1.0 * weight / ((1.0 * height) * (1.0 * height))) * 703;
+            //myBMI = (weight / (height * height)) * 703;
+            return myBMI;
+        }
+
+
+        public result myHealthJson(int height, int weight)
+        {
+            Service1 s = new Service1();
+            result r = new result();
+
+            double bmi;
+            string risk;
+            string more;
+
+            bmi = s.myBMI(height, weight);
+
+            if (bmi < 18)
+            {
+                risk = "You are underweight";
+            }
+            else if (bmi >= 18 & bmi < 25)
+            {
+                risk = "You are normal";
+            }
+
+            else if (bmi >= 25 & bmi <= 30)
+            {
+                risk = "You are pre-obese";
+            }
+            else //(bmi > 30)
+            {
+                risk = "You are obses";
+            }
+
+            more =
+                "https://www.cdc.gov/healthyweight/assessing/bmi/index.html, " + "\r\n" +
+                "https://www.nhlbi.nih.gov/health/educational/lose_wt/index.htm, " + "\r\n" +
+                "https://www.ucsfhealth.org/education/body_mass_index_tool/";
+
+
+            r.bmi = bmi;
+            r.risk = risk;
+            r.more = more;
+            return r;
+
+
+
+        }
+
+        public result myHealthXml(int height, int weight)
+        {
+            Service1 s = new Service1();
+            result r = new result();
+
+            double bmi;
+            string risk;
+            string more;
+
+            bmi = s.myBMI(height, weight);
+
+            if (bmi < 18)
+            {
+                risk = "You are underweight";
+            }
+            else if (bmi >= 18 & bmi < 25)
+            {
+                risk = "You are normal.";
+            }
+
+            else if (bmi >= 25 & bmi <= 30)
+            {
+                risk = "You are pre-obese";
+            }
+            else //(bmi > 30)
+            {
+                risk = "You are obses";
+            }
+
+            more =
+                "https://www.cdc.gov/healthyweight/assessing/bmi/index.html, " + "\r\n" +
+                "https://www.nhlbi.nih.gov/health/educational/lose_wt/index.htm, " + "\r\n" +
+                "https://www.ucsfhealth.org/education/body_mass_index_tool/";
+
+
+            r.bmi = bmi;
+            r.risk = risk;
+            r.more = more;
+            return r;
+
+
+
+        }
+
+
+    }
+}
